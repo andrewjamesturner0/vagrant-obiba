6d1906ae3e3231fbeef32f490b16e58cff4cdd36
Andrew Turner <andrewjamesturner0@gmail.com>
2014-04-01 15:27:22 +0100

    Borrow install-opal.sh and make it standalone
    * install-opal.sh: change VAGRANT_DATA path so script runs without vagrant
    * Change what R bits are installed: no clientside packages (e.g. RStudio)
    * Change how R datashield packages are installed
    
    **CHANGES ARE NOT FOR UPSTREAM**

--- data/r/install-opal-r-server.R
+++ data/r/install-opal-r-server.R
@@ -1,7 +1,4 @@
 #! /usr/bin/Rscript --vanilla
 
-# Install datashield server R packages via opal
-library(opaladmin)
-o<-opal.login('administrator', 'password', url='https://localhost:8443')
-dsadmin.install_package(o, 'datashield')
-opal.logout(o)
+install.packages('datashield', repos='http://cran.obiba.org', type='source')
+install.packages('devtools', repos='http://cran.uk.r-project.org')
