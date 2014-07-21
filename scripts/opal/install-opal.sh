55d67ad5c5f32c34a995a01fe65fd7499ecfb1c1
Andrew Turner <andrewjamesturner0@gmail.com>
2014-07-21 13:11:05 +0100

    comment out install-opal-r-server.R

--- scripts/opal/install-opal.sh
+++ scripts/opal/install-opal.sh
@@ -55,7 +55,7 @@ sudo service rserver restart
 
 # Opal Datashield
 #sudo Rscript $VAGRANT_DATA/r/install-opal-r-client.R
-sudo Rscript $VAGRANT_DATA/r/install-opal-r-server.R
+#sudo Rscript $VAGRANT_DATA/r/install-opal-r-server.R
 
 # R studio
 #sudo apt-get -y install libapparmor1
